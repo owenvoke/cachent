2344b9cd858469d8914f11f9f3cd28e42e234367
PXgamer <owzie123@gmail.com>
2017-09-04 23:34:13 +0100

    Added lang/en/cache

---
+++ resources/lang/en/cache.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'access_url_structure' => 'To access torrents stored in this database, you can use the following URL structure:',
+    'cache_torrent' => 'Cache a torrent file',
+    'no_search' => 'You can not search or list torrent files that are stored here, you can only access them if you already know the info_hash value of the torrent you want to download.',
+    'accessing_cached_torrents' => 'Accessing cached torrents',
+];
