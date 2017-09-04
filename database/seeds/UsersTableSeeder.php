a682f6c110c24dcfbfdcd955ffb9ee1014766b36
PXgamer <owzie123@gmail.com>
2017-09-04 21:59:15 +0100

    Added UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $username = 'admin';
+        $email = 'admin@cachent';
+        $password = str_random(25);
+
+        $output = new ConsoleOutput();
+        $output->writeln([
+            '',
+            'Email: ' . $email,
+            'Password: ' . $password
+        ]);
+
+        DB::table('users')->insert([
+            'username' => $username,
+            'email' => $email,
+            'password' => bcrypt($password)
+        ]);
+    }
+}
