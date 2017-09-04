c2afb2dc93f15e546cc66bc4a0ef762ec66541e4
PXgamer <owzie123@gmail.com>
2017-09-04 22:19:28 +0100

    Force torrents.hash to be unique

--- database/migrations/2017_09_04_210415_create_torrents_table.php
+++ database/migrations/2017_09_04_210415_create_torrents_table.php
@@ -15,7 +15,7 @@ class CreateTorrentsTable extends Migration
     {
         Schema::create('torrents', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('hash');
+            $table->string('hash')->unique();
 
             $table->softDeletes();
             $table->timestamps();
