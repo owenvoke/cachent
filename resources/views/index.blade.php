bed86f96311246b95f2c32ed096298f44b308c6c
PXgamer <owzie123@gmail.com>
2017-09-05 13:18:25 +0100

    Split up index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,20 +19,7 @@
         <div>
             <h3>@lang('cache.cache_torrent')</h3>
 
-            <form action="{{ route('torrents.store') }}" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <div class="btn-group">
-                        <label class="btn btn-default btn-wide btn-text-left" for="file">
-                            <span>Choose files...</span>
-                            <input class="hide" name="file" id="file" type="file">
-                        </label>
-                        <button class="btn btn-default btn-primary">
-                            <span>Cache</span>
-                        </button>
-                    </div>
-                </div>
-            </form>
+            @include('torrents.form')
         </div>
     </div>
 @endsection
