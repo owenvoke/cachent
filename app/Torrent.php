a800d5bd743f54eec729be6fea22d1e9f922e01a
PXgamer <owzie123@gmail.com>
2017-09-05 14:48:00 +0100

    Added new method ::addFileToDatabase() to Torrent model

--- app/Torrent.php
+++ app/Torrent.php
@@ -4,10 +4,41 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
+/**
+ * @property int $id
+ * @property string $hash
+ * @property string deleted_at
+ * @property string created_at
+ * @property string updated_at
+ */
 class Torrent extends Model
 {
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
+
+    protected $status = false;
+
+    public function addFileToDatabase(UploadedFile $file)
+    {
+        if ($file->extension() === 'torrent') {
+            $torrent = new \Torrent($file->getRealPath());
+            $this->hash = $torrent->hash_info();
+
+            if ($this->hash) {
+                if (!Storage::exists('torrents/' . $this->hash . '.torrent')) {
+                    $this->status = $file->storeAs('torrents', $this->hash . '.torrent');
+
+                    return $this->save();
+                } else {
+                    $this->status = true;
+                }
+            }
+        }
+
+        return $this->status;
+    }
 }
