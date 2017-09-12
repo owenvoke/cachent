c40da59ceb44ef15fb6fed9f8cbf00e7e38590ba
PXgamer <owzie123@gmail.com>
2017-09-12 20:35:39 +0100

    Added StatisticsController::show() method

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Torrent;
+use Illuminate\Support\Facades\Storage;
 
 class StatisticController extends Controller
 {
@@ -16,5 +17,29 @@ class StatisticController extends Controller
         ];
 
         return view('statistics.index', $data);
+    }
+
+    /**
+     * @param string $hash
+     * @return \Illuminate\Http\Response
+     */
+    public function show(string $hash)
+    {
+        $torrent = Torrent::where('hash', $hash)->first();
+
+        $storagePath = 'torrents/' . $hash . '.torrent';
+
+        if ($torrent->hash && Storage::exists($storagePath)) {
+            $data = [
+                'torrent' => $torrent,
+                'file'    => new \SplFileInfo(storage_path('app/') . $storagePath)
+            ];
+
+            return view('torrents.show', $data);
+        } else {
+            return response()->json([
+                'error' => 'File not found'
+            ])->setStatusCode(404);
+        }
     }
 }
