210de03d1e30e35bdb914b3054082e4cc7daf1cf
PXgamer <owzie123@gmail.com>
2017-09-12 21:09:15 +0100

    Added statistics.show route link to torrents._list view

--- resources/views/torrents/_list.blade.php
+++ resources/views/torrents/_list.blade.php
@@ -1,24 +1,35 @@
 @forelse ($torrents as $torrent)
     <div class="row" data-id="{{ $torrent->hash }}">
-        <div class="col-md-10">
+        <div class="col-md-9">
             <a href="{{ route('torrents.show', $torrent->hash) }}">
                 <strong class="ellipsis">{{ $torrent->hash }}</strong>
             </a>
         </div>
-        <div class="col-md-2">
-            <div class="soft-delete">
-                @if ($torrent->trashed())
-                    <div title="Restore" class="btn btn-success btn-xs">
-                        <span class="glyphicon glyphicon-ok-circle"></span>
-                        <span>Restore</span>
+        <div class="col-md-3">
+            <ul class="list-inline list-unstyled">
+                <li>
+                    <a href="{{ route('statistics.show', ['hash'=>$torrent->hash]) }}"
+                       title="Statistics" class="btn btn-primary btn-xs">
+                        <span class="glyphicon glyphicon-stats"></span>
+                        <span>Statistics</span>
+                    </a>
+                </li>
+                <li>
+                    <div class="soft-delete">
+                        @if ($torrent->trashed())
+                            <div title="Restore" class="btn btn-success btn-xs">
+                                <span class="glyphicon glyphicon-ok-circle"></span>
+                                <span>Restore</span>
+                            </div>
+                        @else
+                            <div title="Soft Delete" class="btn btn-danger btn-xs">
+                                <span class="glyphicon glyphicon-ban-circle"></span>
+                                <span>Delete</span>
+                            </div>
+                        @endif
                     </div>
-                @else
-                    <div title="Soft Delete" class="btn btn-danger btn-xs">
-                        <span class="glyphicon glyphicon-ban-circle"></span>
-                        <span>Soft Delete</span>
-                    </div>
-                @endif
-            </div>
+                </li>
+            </ul>
         </div>
     </div>
     <hr>
