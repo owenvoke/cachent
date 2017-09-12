d77eb919c891bb45eb6a616beb3729518008a9e7
PXgamer <owzie123@gmail.com>
2017-09-12 10:15:05 +0100

    Added StatisticController class for #3
    Currently displays:
    - Download Statistics
    - Most Downloaded Torrent
    - Total Torrents

---
+++ app/Http/Controllers/StatisticController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Torrent;
+
+class StatisticController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'totalDownloads' => Torrent::getTotalDownloads(),
+            'mostDownloaded' => Torrent::orderBy('downloads', 'desc')->first(),
+            'totalTorrents'  => Torrent::count(),
+            'totalTorrentsWithDeleted'  => Torrent::withTrashed()->count(),
+        ];
+
+        return view('statistics.index', $data);
+    }
+}
