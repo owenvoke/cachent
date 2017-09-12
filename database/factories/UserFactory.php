cc3f23da7d9738b05a9a0900759ec7e8a422d200
PXgamer <owzie123@gmail.com>
2017-09-12 09:35:44 +0100

    Updated UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,13 +13,14 @@ use Faker\Generator as Faker;
 |
 */
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
