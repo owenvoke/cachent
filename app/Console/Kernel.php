663bf4737ab4ac1eb02f585baef8b39e827d985f
pxgamer <owzie123@gmail.com>
2017-10-31 22:43:47 +0000

    Added Commands\Purge to the Kernel::$commands array

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,19 +13,17 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\Purge::class
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
     }
 
     /**
@@ -35,7 +33,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
