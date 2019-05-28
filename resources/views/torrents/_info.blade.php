123e9b657012319c80915d32857e67f9da48836a
pxgamer <owzie123@gmail.com>
2019-05-28 10:52:35 +0100

    Remove the deprecated Blade 'or' operator

--- resources/views/torrents/_info.blade.php
+++ resources/views/torrents/_info.blade.php
@@ -28,15 +28,15 @@
             </tr>
             <tr>
                 <th>First added</th>
-                <td class="ellipsis">{{ $torrent->created_at or date('Y-m-d H:i:s') }}</td>
+                <td class="ellipsis">{{ $torrent->created_at ?? date('Y-m-d H:i:s') }}</td>
             </tr>
             <tr>
                 <th>Updated at</th>
-                <td class="ellipsis">{{ $torrent->updated_at or date('Y-m-d H:i:s') }}</td>
+                <td class="ellipsis">{{ $torrent->updated_at ?? date('Y-m-d H:i:s') }}</td>
             </tr>
             <tr>
                 <th>Downloads</th>
-                <td class="ellipsis">{{ $torrent->downloads or 0 }}</td>
+                <td class="ellipsis">{{ $torrent->downloads ?? 0 }}</td>
             </tr>
         </table>
     </div>
