ea6ec82bb08a13ec4dea94028396cec7b39fa3f4
Owen Voke <development@voke.dev>
2023-06-06 19:21:18 +0100

    chore: remove Dashboard nav link

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -9,15 +9,6 @@
                         <x-application-mark class="block h-9 w-auto text-purple-600 dark:text-purple-300" />
                     </a>
                 </div>
-
-                @auth
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-                @endauth
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -150,12 +141,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             @auth
