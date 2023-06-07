9280539a3e38d7ed7663e5115bbba81cc72efe94
Owen Voke <development@voke.dev>
2023-06-07 13:39:29 +0100

    fix: add missing auth nav on mobile

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="flex flex-col sm:justify-center items-center py-12 bg-gray-50 dark:bg-gray-900">
+<div class="flex flex-col sm:justify-center items-center py-12 px-2 bg-gray-50 dark:bg-gray-900">
     <div class="text-purple-600 dark:text-purple-300">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden rounded-lg">
         {{ $slot }}
     </div>
 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -212,6 +212,15 @@
                     @endif
                 @endif
             </div>
+            @elseif(Route::has('login'))
+                <x-responsive-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+                    {{ __('Login') }}
+                </x-responsive-nav-link>
+                @if (Route::has('register'))
+                    <x-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                        {{ __('Register') }}
+                    </x-responsive-nav-link>
+                @endif
             @endauth
         </div>
     </div>
